2b17fade0642728aec16834920587ffeb8980b1d
Wolff <hutingrui01@yeah.net>
2025-01-28 12:30:36 +0800

    feat: 基建

---
+++ src/global-imports.ts
@@ -0,0 +1,6 @@
+/**
+ * 这个目录放默认要自动（由 unplugin-auto-import 执行）全局导入的模块。
+ */
+export * as _ from "lodash-es"
+export { clone as cloneShallow, cloneDeep as clone } from "lodash-es"
+// export { default as router, route, onViewPass, useViewRouteEnter } from "@/router"
---
+++ vite.config.ts
@@ -0,0 +1,77 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import UnpluginAutoImport from 'unplugin-auto-import/vite'
+import UnpluginVueComponents from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import * as path from "path"
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+    const base_config = {
+        base: './',
+        resolve:
+        {
+            alias:
+            {
+                '@': path.resolve(__dirname, "./src"),
+            },
+        },
+        server:
+        {
+            host: '0.0.0.0',
+            port: 8080,
+        },
+        plugins:
+        [
+            vue(),
+            UnpluginAutoImport({
+                dts: true,
+                eslintrc: {
+                    enabled: true,
+                },
+                imports: [
+                    // presets //
+                    'vue',
+                    // 'vue-router',
+                    // global-imports //
+                    {
+                        '@/global-imports':
+                        [
+                            '_',
+                            'clone',
+                            'cloneShallow',
+                            // 'router',
+                            // 'route',
+                        ],
+                    },
+                ],
+                resolvers: [ElementPlusResolver()],
+            }),
+            UnpluginVueComponents({
+                resolvers: [ElementPlusResolver()],
+            }),
+        ],
+        css:
+        {
+            preprocessorOptions:
+            {
+                stylus:
+                {
+                    globals:
+                    {
+                        '_colorset': path.resolve('src/assets/stylesheets/colorset/index.styl'),
+                        '_patterns': path.resolve('src/assets/stylesheets/patterns/index.styl'),
+                    },
+                },
+            },
+        },
+        define:
+        {
+            // __APP_ENV__: 'dev',
+        } as Record<string, string>,
+    }
+
+    return base_config
+})
